12b8b1d13368de3d79797e57ba1886a5beee5900
mohan <mohankumar03366@gmail.com>
2024-02-21 18:18:37 +0530

    axios information

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <>lms</>;
+  return (
+    <>
+      lms
+      <Routes></Routes>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,16 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Store from "./Redux/Store.js";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={Store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
+);
